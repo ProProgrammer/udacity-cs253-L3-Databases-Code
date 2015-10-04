12dc48257f34d0ba98cdf2931683b873d17a6113
Deep Sukhwani <deepsukhwani@gmail.com>
2015-10-04 16:13:43 +0530

    L3 > Form Handling & Form Handling Continued

--- asciichan.py
+++ asciichan.py
@@ -20,7 +20,24 @@ class Handler(webapp2.RequestHandler):
 		self.write(self.render_str(template, **kw))
 
 class MainPage(Handler):
+	def render_front(self, title="", artwork="", error=""):
+		"""
+		This function is created to avoid code duplication as we are going to render form using "self.render" quite a few times
+		"""
+		self.render("front.html", title=title, artwork=artwork, error=error)
+
 	def get(self):
-		self.write('asciichan')
+		# self.render("front.html")
+		self.render_front()
+
+	def post(self):
+		title = self.request.get('title')
+		art = self.request.get('art')
+
+		if title and art:
+			self.write('Thanks!')
+		else:
+			error = "We need both, a title and an artwork!"
+			self.render_front(title=title, artwork=art, error=error)
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
